c7d2f8200753ce2c1bb00d56e5d55664ff451545
David Fonseca <david.fonseca@elkay.com>
2022-03-16 17:32:05 -0600

    Add folder shared(buttons, controls, indicators, layouts, popups and utils) cap 50

---
+++ src/app/Shared/index.ts
@@ -0,0 +1,7 @@
+//exportacion de cada carpeta de elemntos graficos
+
+export * from './buttons'; //busca el index de la carpeta buttons y así de cada uno
+export * from './controls';
+export * from './popups';
+export * from './layouts';
+export * from './indicators';
---
+++ src/app/Shared/indicators/index.ts
@@ -0,0 +1,2 @@
+//Exporta todos los recursos que se encinetren dentro de este modulo
+export * from './indicators.module';

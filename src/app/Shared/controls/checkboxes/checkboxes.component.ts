282bd22ee25d0a807d326e8adcf491c45d9cfb6f
David Fonseca <fonsecasoundcruz@gmail.com>
2022-06-12 00:16:41 -0500

    Add checkboxes part 2 cap.61

--- src/app/Shared/controls/checkboxes/checkboxes.component.ts
+++ src/app/Shared/controls/checkboxes/checkboxes.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  forwardRef,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
@@ -35,15 +42,44 @@ export class CheckboxesComponent implements OnInit, ControlValueAccessor {
   }
 
   registerOnChange(fn: any): void {
-    
     this.propagateChange = fn;
   }
-  registerOnTouched(fn: any): void {
-    
+  registerOnTouched(fn: any): void {}
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
   }
 
+  //dispara al click al checkbox
+  onChanged(value: Value, checked: Event): void {
+    const { target } = checked;
+    const resultado = (target as HTMLInputElement).checked; //si esta activo o no
 
-  setDisabledState(isDisabled : boolean): void{
-    this.isDisabled = isDisabled
+    const selected = this.getSelected(value, resultado);
+
+    this.value = selected;
+    this.propagateChange(selected);
+    this.changed.emit(selected);
+  }
+
+  private getSelected(value: Value, checked: boolean): Value[] {
+    const selected: Value[] = this.value ? [...this.value] : [];
+
+    if (checked) {
+      if (!selected.includes(value)) {
+        //si es chequeado
+        selected.push(value);
+      } else {
+        //que nmo este chequeado
+        const index = selected.indexOf(value);
+        //lo elimina de la colección
+        selected.splice(index, 1);
+      }
+    } //si tiene elementos regresalos, si no manda un arreglo vacio
+    return selected.length ? selected : [];
+  }
+
+  isChecked(value: Value): boolean {
+    return this.value && this.value.includes(value);
   }
 }
